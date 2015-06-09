bbce80d1fd14a64212f1ce734107e2d18246640c
Bryce Stradling <bryce@getvyral.com>
2015-06-09 15:52:25 -0500

    adjusting gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,69 +5,78 @@ var autoprefixer = require('gulp-autoprefixer');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var autowatch = require('gulp-autowatch');
+var gls = require('gulp-live-server');
 
 // jade
 gulp.task('templates', function() {
   var YOUR_LOCALS = {};
  
-  gulp.src('./jade/*.jade')
+  gulp.src('./src/jade/**/*.jade')
     .pipe(jade({
       locals: YOUR_LOCALS
     }))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./public/html/'))
 });
 
 // Get one .styl file and render 
 gulp.task('stylus', function () {
-  gulp.src('./stylus/main.styl')
+  gulp.src('./src/stylus/main.styl')
     .pipe(stylus())
-    .pipe(gulp.dest('./css/'));
+    .pipe(gulp.dest('./public/css/'));
 });
 
 // autoprefixer
 gulp.task('autoprefix', function () {
-    return gulp.src('./css/')
+    return gulp.src('./public/css/')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('./css/'));
+        .pipe(gulp.dest('./public/css/'));
 });
 
 // compress 
 gulp.task('compress', function () {
-  gulp.src('./stylus/main.styl')
+  gulp.src('./src/stylus/main.styl')
     .pipe(stylus({
       compress: true
     }))
-    .pipe(gulp.dest('./css/'));
+    .pipe(gulp.dest('./public/css/'));
 });
 
 //imagemin
 
 gulp.task('imagemin', function () {
-    return gulp.src('./img/*')
+    return gulp.src('./public/img/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('img'));
+        .pipe(gulp.dest('./public/img'));
 });
 
 // key = task name to run 
 // value = glob or array of globs to watch 
 var paths = {
-  stylus: './stylus/**/*.styl',
-  jade: './jade/**/*.html',
+  stylus: 'src/**/*.styl',
+  jade: 'src/**/*.jade',
+  js: 'public/**/*.js',
 };
 
 gulp.task('watch', function() {
   autowatch(gulp, paths);
 });
 
+//Live-server
+gulp.task('serve', function() {
+    //1. serve with default settings 
+    var server = gls.static('public', 3000); //equals to gls.static('public', 3000); 
+    server.start();
+   //watch
+    gulp.watch(['./public/**/*.css', './public/**/*.html'], function () {
+        server.notify.apply(server, arguments);
+    });
+});
 
-
-
-
-gulp.task('default', ['templates', 'stylus', 'autoprefix', 'compress', 'imagemin','watch']);
+gulp.task('default', ['templates', 'stylus', 'autoprefix', 'compress', 'imagemin','watch', 'serve']);
